21704687728a900bf2ad5befd98b4c05a51dffb0
Akmal Sidik <143138356+WP2-AkmalSidik@users.noreply.github.com>
2025-01-03 19:07:15 +0700

    feat(frontend): menambahkan untuk semua tampilan

--- start/routes.ts
+++ start/routes.ts
@@ -9,4 +9,24 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.get('/login', async ({ view }) => {
+  return view.render('auth/login-page')
+})
+router.get('/profile', async ({ view }) => {
+  return view.render('pages/pasien/profile-pasien')
+})
+router.get('/', async ({ view }) => {
+  return view.render('pages/index')
+})
+router.get('/data-pasien', async ({ view }) => {
+  return view.render('pages/pasien/data-pasien')
+})
+router.get('/data-obat', async ({ view }) => {
+  return view.render('pages/pasien/data-obat')
+})
+router.get('/data-kunjungan', async ({ view }) => {
+  return view.render('pages/pasien/data-kunjungan')
+})
+router.get('/hasil-lab', async ({ view }) => {
+  return view.render('pages/pasien/hasil-lab')
+})
