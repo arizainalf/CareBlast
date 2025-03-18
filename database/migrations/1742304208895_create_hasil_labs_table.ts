98db856b942952f45d0cd610a27f55bf8746d01f
arizainalf <arizainalf@gmail.com>
2025-03-18 22:29:30 +0700

    fix(whatsapp_service): fix error bagian saveMessages

--- database/migrations/1742304208895_create_hasil_labs_table.ts
+++ database/migrations/1742304208895_create_hasil_labs_table.ts
@@ -6,7 +6,11 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
+      table.string('wa_id')
+      table.string('name')
+      table.string('file_name')
+      table.string('file_path')
+      table.string('caption').nullable()
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
